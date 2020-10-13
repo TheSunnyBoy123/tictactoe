b62d17b64d5a70da85c85f36a30cbaf9b097b1e6
unknown <sanchay2357@gmail.com>
2020-10-13 19:09:21 +0530

    man so this is defining a function

--- testSetup.py
+++ testSetup.py
@@ -1,3 +1,9 @@
 print("Testing tictactoe git setup")
 print("man this is the hardest thing i have ever done")
 man i didnt study anything in chem today!!! nice
+def sum (a,b):
+    return a + b
+
+x=10
+y=20
+sum(x,y)
